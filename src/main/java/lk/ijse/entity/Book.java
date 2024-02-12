ae2aa264fd6b318f53558c62184ebe6d8d9de25b
LRLayan <rameshlayan4@gmail.com>
2024-02-12 22:13:02 +0530

    properties , NoArgsConstructor , AllArgsConstructor , Getter and Setter

--- src/main/java/lk/ijse/entity/Book.java
+++ src/main/java/lk/ijse/entity/Book.java
@@ -1,4 +1,15 @@
 package lk.ijse.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
+    private int id;
+    private String title;
+    private String publicationYear;
+    private double price;
 }
