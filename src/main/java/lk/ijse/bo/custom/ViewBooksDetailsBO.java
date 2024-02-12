82ae6fb4e34dbfdccf4d8b3a10fbebc4fdcc4966
LRLayan <rameshlayan4@gmail.com>
2024-02-13 03:05:52 +0530

    change interface name

--- src/main/java/lk/ijse/bo/custom/ViewBooksDetailsBO.java
+++ src/main/java/lk/ijse/bo/custom/ViewBooksDetailsBO.java
@@ -2,11 +2,10 @@ package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBO;
 import lk.ijse.dto.AuthorDTO;
-import lk.ijse.entity.Author;
 
 import java.util.ArrayList;
 
-public interface AuthorBO extends SuperBO {
+public interface ViewBooksDetailsBO extends SuperBO {
     boolean saveAuthor();
     boolean updateAuthor();
     boolean deleteAuthor();
