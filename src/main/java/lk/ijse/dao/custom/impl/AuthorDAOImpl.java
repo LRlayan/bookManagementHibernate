bf9ab587b451f25faa511acde81df640a93b20e6
LRLayan <rameshlayan4@gmail.com>
2024-02-12 22:34:11 +0530

    implement AuthorDAO interface and Override method

---
+++ src/main/java/lk/ijse/dao/custom/impl/AuthorDAOImpl.java
@@ -0,0 +1,28 @@
+package lk.ijse.dao.custom.impl;
+
+import lk.ijse.dao.custom.AuthorDAO;
+import lk.ijse.entity.Author;
+
+import java.util.ArrayList;
+
+public class AuthorDAOImpl implements AuthorDAO {
+    @Override
+    public boolean update() {
+        return false;
+    }
+
+    @Override
+    public boolean save() {
+        return false;
+    }
+
+    @Override
+    public boolean delete() {
+        return false;
+    }
+
+    @Override
+    public ArrayList<Author> findAll() {
+        return null;
+    }
+}
