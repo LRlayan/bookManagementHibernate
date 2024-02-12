cf9801f57770562dd4d87ada70ba78ab266a242c
LRLayan <rameshlayan4@gmail.com>
2024-02-13 00:30:19 +0530

    stackoverflow bug fix

--- src/main/java/lk/ijse/AppInitializerWrapper.java
+++ src/main/java/lk/ijse/AppInitializerWrapper.java
@@ -2,6 +2,6 @@ package lk.ijse;
 
 public class AppInitializerWrapper {
     public static void main(String[] args) {
-        AppInitializerWrapper.main(args);
+        AppInitializer.main(args);
     }
 }
