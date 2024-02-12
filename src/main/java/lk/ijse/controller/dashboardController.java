ae07a71b527f6ea1b69463a525b0bbb72d7ed6f5
LRLayan <rameshlayan4@gmail.com>
2024-02-13 01:58:23 +0530

    remove code

--- src/main/java/lk/ijse/controller/dashboardController.java
+++ src/main/java/lk/ijse/controller/dashboardController.java
@@ -1,4 +1,25 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import lk.ijse.pages.Impl.PagesImpl;
+import lk.ijse.pages.Pages;
+
+import java.io.IOException;
+
 public class dashboardController {
+
+    @FXML
+    private AnchorPane mainAnchorpane;
+    @FXML
+    private JFXButton button;
+
+    Pages pages = new PagesImpl();
+
+    public void goToHomeBtnOnAction(ActionEvent actionEvent) throws IOException {
+        pages.popUpWindow("/view/HomePageForm.fxml");
+        pages.closeWindow(button);
+    }
 }
