37861e532aa6a4f73dc4c09e4cffeccc4db8c84c
LRLayan <rameshlayan4@gmail.com>
2024-02-12 22:42:35 +0530

    implement AuthorBO interface and Override method

--- src/main/java/lk/ijse/bo/custom/impl/AuthorBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/AuthorBOImpl.java
@@ -1,4 +1,28 @@
 package lk.ijse.bo.custom.impl;
 
-public class AuthorBOImpl {
+import lk.ijse.bo.custom.AuthorBO;
+import lk.ijse.dto.AuthorDTO;
+
+import java.util.ArrayList;
+
+public class AuthorBOImpl implements AuthorBO {
+    @Override
+    public boolean saveAuthor() {
+        return false;
+    }
+
+    @Override
+    public boolean updateAuthor() {
+        return false;
+    }
+
+    @Override
+    public boolean deleteAuthor() {
+        return false;
+    }
+
+    @Override
+    public ArrayList<AuthorDTO> getAllAuthors() {
+        return null;
+    }
 }
