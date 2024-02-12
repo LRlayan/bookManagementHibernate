a16be8436bcabf3340fe5f984c9f561f2c5388b3
LRLayan <rameshlayan4@gmail.com>
2024-02-13 04:06:45 +0530

    add annotation in hibernate

--- src/main/java/lk/ijse/controller/HomePageFormController.java
+++ src/main/java/lk/ijse/controller/HomePageFormController.java
@@ -4,8 +4,14 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import lk.ijse.config.FactoryConfiguration;
+import lk.ijse.dto.AuthorDTO;
+import lk.ijse.dto.BookDTO;
 import lk.ijse.pages.Impl.PagesImpl;
 import lk.ijse.pages.Pages;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.io.IOException;
 
@@ -14,20 +20,56 @@ public class HomePageFormController {
     @FXML
     private JFXButton viewBtn;
 
+    @FXML
+    private TextField txtAuthorName;
+
+    @FXML
+    private TextField txtBookTitle;
+
+    @FXML
+    private TextField txtPrice;
+
+    @FXML
+    private TextField txtPublishedYear;
+
     Pages pages = new PagesImpl();
 
-    @FXML
-    void saveAuthorOnAction(ActionEvent event) {
 
-    }
-
-    @FXML
-    void saveBookOnAction(ActionEvent event) {
-
-    }
 
     @FXML
     void viewBookDetailsOnAction(ActionEvent event) throws IOException {
         pages.popUpWindow("/view/viewBooksDetailsForm.fxml");
     }
+
+    @FXML
+    public void saveRecordOnAction(ActionEvent actionEvent) {
+
+        var authorDTO = new AuthorDTO();
+        authorDTO.setName(txtAuthorName.getText());
+
+        var bookDTO = new BookDTO();
+        bookDTO.setTitle(txtBookTitle.getText());
+        bookDTO.setPublicationYear(txtPublishedYear.getText());
+        bookDTO.setPrice(Double.parseDouble(txtPrice.getText()));
+    }
+
+//    public void saveRecord(String author, String bookTitle, String publishedYear, double price){
+//
+//        var authorDTO = new AuthorDTO();
+//        authorDTO.setName(author);
+//
+//        var bookDTO = new BookDTO();
+//        bookDTO.setTitle(bookTitle);
+//        bookDTO.setPublicationYear(publishedYear);
+//        bookDTO.setPrice(price);
+//
+//        Session session = FactoryConfiguration.getInstance().getSession();
+//        Transaction transaction = session.beginTransaction();
+//
+//        session.save(authorDTO);
+//        session.save(bookDTO);
+//
+//        transaction.commit();
+//        session.close();
+
 }
--- src/main/java/lk/ijse/entity/Book.java
+++ src/main/java/lk/ijse/entity/Book.java
@@ -1,15 +1,24 @@
 package lk.ijse.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private String publicationYear;
     private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
 }
