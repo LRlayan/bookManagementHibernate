513da90ddf8428d67fdc4443168bbab365e5e8a3
LRLayan <rameshlayan4@gmail.com>
2024-02-12 22:12:55 +0530

    properties , NoArgsConstructor , AllArgsConstructor , Getter and Setter

--- src/main/java/lk/ijse/entity/Author.java
+++ src/main/java/lk/ijse/entity/Author.java
@@ -1,4 +1,13 @@
 package lk.ijse.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
+    private int id;
+    private String name;
 }
