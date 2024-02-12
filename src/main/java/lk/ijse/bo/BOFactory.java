f15a451bda70133cd34a0277c0ae67c69906956d
LRLayan <rameshlayan4@gmail.com>
2024-02-12 23:53:12 +0530

    implementation BOFactory

--- src/main/java/lk/ijse/bo/BOFactory.java
+++ src/main/java/lk/ijse/bo/BOFactory.java
@@ -1,4 +1,31 @@
 package lk.ijse.bo;
 
+import lk.ijse.bo.custom.impl.AuthorBOImpl;
+import lk.ijse.bo.custom.impl.BookBOImpl;
+import lk.ijse.bo.custom.impl.DashboardImpl;
+
 public class BOFactory {
+    private static BOFactory boFactory;
+    private BOFactory(){}
+
+    public static BOFactory getBOFactory(){
+        return (boFactory == null) ? boFactory = new BOFactory() : boFactory;
+    }
+
+    public enum BOTypes{
+        DASHBOARD,AUTHOR,BOOKS
+    }
+
+    public SuperBO boTypes(BOTypes boTypes){
+        switch (boTypes){
+            case BOOKS:
+                return new BookBOImpl();
+            case AUTHOR:
+                return new AuthorBOImpl();
+            case DASHBOARD:
+                return new DashboardImpl();
+            default:
+                return null;
+        }
+    }
 }
