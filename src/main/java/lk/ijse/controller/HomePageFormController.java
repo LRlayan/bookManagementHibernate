c5d9943c9957cf54c352d3a0f5331bddd73a3cc7
LRLayan <rameshlayan4@gmail.com>
2024-02-13 03:07:10 +0530

    viewBookDetailsOnAction() and others method create

--- src/main/java/lk/ijse/controller/HomePageFormController.java
+++ src/main/java/lk/ijse/controller/HomePageFormController.java
@@ -1,8 +1,9 @@
 package lk.ijse.controller;
 
+
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
 import lk.ijse.pages.Impl.PagesImpl;
 import lk.ijse.pages.Pages;
 
@@ -10,4 +11,23 @@ import java.io.IOException;
 
 public class HomePageFormController {
 
+    @FXML
+    private JFXButton viewBtn;
+
+    Pages pages = new PagesImpl();
+
+    @FXML
+    void saveAuthorOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewBookDetailsOnAction(ActionEvent event) throws IOException {
+        pages.popUpWindow("/view/viewBooksDetailsForm.fxml");
+    }
 }
