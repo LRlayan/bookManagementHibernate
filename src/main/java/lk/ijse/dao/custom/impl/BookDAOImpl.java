68d50003bdbc44033cd9320725328008622e5da0
LRLayan <rameshlayan4@gmail.com>
2024-02-12 22:32:17 +0530

    implement BookDAO interface and Override method

--- src/main/java/lk/ijse/dao/custom/impl/BookDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/BookDAOImpl.java
@@ -1,4 +1,28 @@
 package lk.ijse.dao.custom.impl;
 
-public class BookDAOImpl {
+import lk.ijse.dao.custom.BookDAO;
+import lk.ijse.entity.Book;
+
+import java.util.ArrayList;
+
+public class BookDAOImpl implements BookDAO {
+    @Override
+    public boolean update() {
+        return false;
+    }
+
+    @Override
+    public boolean save() {
+        return false;
+    }
+
+    @Override
+    public boolean delete() {
+        return false;
+    }
+
+    @Override
+    public ArrayList<Book> findAll() {
+        return null;
+    }
 }
