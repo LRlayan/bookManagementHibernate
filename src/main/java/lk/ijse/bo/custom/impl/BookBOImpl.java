81d9c8580d8c7699bed85fc45aea3379d7369b1a
LRLayan <rameshlayan4@gmail.com>
2024-02-12 22:42:43 +0530

    implement BookBO interface and Override method

--- src/main/java/lk/ijse/bo/custom/impl/BookBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/BookBOImpl.java
@@ -1,4 +1,28 @@
 package lk.ijse.bo.custom.impl;
 
-public class BookBOImpl {
+import lk.ijse.bo.custom.BookBO;
+import lk.ijse.dto.BookDTO;
+
+import java.util.ArrayList;
+
+public class BookBOImpl implements BookBO {
+    @Override
+    public boolean saveBook() {
+        return false;
+    }
+
+    @Override
+    public boolean updateBook() {
+        return false;
+    }
+
+    @Override
+    public boolean deleteBook() {
+        return false;
+    }
+
+    @Override
+    public ArrayList<BookDTO> getAllBooks() {
+        return null;
+    }
 }
