559040296db17ceba2b9e6f6a9eee15fbd456c97
LRLayan <rameshlayan4@gmail.com>
2024-02-13 01:58:55 +0530

    remove code

--- src/main/java/lk/ijse/controller/HomePageFormController.java
+++ src/main/java/lk/ijse/controller/HomePageFormController.java
@@ -1,4 +1,13 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import lk.ijse.pages.Impl.PagesImpl;
+import lk.ijse.pages.Pages;
+
+import java.io.IOException;
+
 public class HomePageFormController {
+
 }
