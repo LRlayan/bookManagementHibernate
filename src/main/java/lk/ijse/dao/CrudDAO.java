eb1ed80ad486836cbba15831fc4004bf74e52486
LRLayan <rameshlayan4@gmail.com>
2024-02-14 01:12:48 +0530

    ArrayList type change List

--- src/main/java/lk/ijse/dao/CrudDAO.java
+++ src/main/java/lk/ijse/dao/CrudDAO.java
@@ -3,10 +3,11 @@ package lk.ijse.dao;
 import lk.ijse.entity.Author;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface CrudDAO<T> extends SuperDAO{
     boolean update();
     boolean save(T entity);
     boolean delete();
-    ArrayList<T> findAll();
+   List<T> findAll();
 }
