4819b16d7bc78e9b9bad1976a6636c4bd565f4ce
LRLayan <rameshlayan4@gmail.com>
2024-02-14 01:13:39 +0530

    implementation findAllAuthor()

--- src/main/java/lk/ijse/bo/custom/impl/HomeBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/HomeBOImpl.java
@@ -10,18 +10,36 @@ import lk.ijse.dto.BookDTO;
 import lk.ijse.entity.Author;
 import lk.ijse.entity.Book;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeBOImpl implements HomeBO {
 
-    AuthorDAO authorDAO = new AuthorDAOImpl();
+   AuthorDAOImpl authorDAO = new AuthorDAOImpl();
     BookDAO bookDAO = new BookDAOImpl();
 
     @Override
     public boolean saveAuthor(AuthorDTO authorDTO) {
-        return authorDAO.save(new Author(authorDTO.getId(),authorDTO.getName()));
+        return authorDAO.save(new Author(authorDTO.getId(),authorDTO.getName(),authorDTO.getCountry()));
     }
 
     @Override
     public boolean saveBook(BookDTO bookDTO) {
         return bookDAO.save(new Book(bookDTO.getId(),bookDTO.getTitle(),bookDTO.getPublicationYear(),bookDTO.getPrice()));
+    }
+
+    @Override
+    public List<AuthorDTO> findAllAuthor() {
+        List<Author> authors = authorDAO.findAll();
+        ArrayList<AuthorDTO> authorDTOS = new ArrayList<>();
+
+        for (Author author : authors){
+//            AuthorDTO authorDTO = new AuthorDTO(author.getId(),author.getName());
+//            authorDTO.setBookList(author.getBookList());
+//            authorDTOS.add(authorDTO);
+            author.setBookList(author.getBookList());
+            authorDTOS.add(new AuthorDTO(author.getId(),author.getName(),author.getCountry(),author.getBookList()));
+        }
+        return authorDTOS;
     }
 }
