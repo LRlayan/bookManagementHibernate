449eca73be4b662004c661cbbb776a26503d0149
LRLayan <rameshlayan4@gmail.com>
2024-02-13 16:18:12 +0530

    save book

--- src/main/java/lk/ijse/dao/custom/impl/BookDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/BookDAOImpl.java
@@ -1,20 +1,40 @@
 package lk.ijse.dao.custom.impl;
 
+import lk.ijse.config.FactoryConfiguration;
+import lk.ijse.dao.custom.AuthorDAO;
 import lk.ijse.dao.custom.BookDAO;
 import lk.ijse.entity.Author;
 import lk.ijse.entity.Book;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.ArrayList;
 
 public class BookDAOImpl implements BookDAO {
+    public static Author author;
     @Override
     public boolean update() {
         return false;
     }
 
     @Override
-    public boolean save(Author author) {
-        return false;
+    public boolean save(Book book) {
+
+        ArrayList<Book> bookList = new ArrayList<>();
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        book.setAuthor(author);
+        author.setBookList(bookList);
+
+        bookList.add(book);
+
+        session.save(book);
+
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
