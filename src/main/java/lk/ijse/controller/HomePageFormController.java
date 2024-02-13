573bc8f6a78a160c24b7a2883c6afbac0c33abce
LRLayan <rameshlayan4@gmail.com>
2024-02-13 15:05:24 +0530

    saveAuthorOnAction() , saveBookOnAction()

--- src/main/java/lk/ijse/controller/HomePageFormController.java
+++ src/main/java/lk/ijse/controller/HomePageFormController.java
@@ -4,7 +4,10 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import lk.ijse.bo.custom.HomeBO;
+import lk.ijse.bo.custom.impl.HomeBOImpl;
 import lk.ijse.config.FactoryConfiguration;
 import lk.ijse.dto.AuthorDTO;
 import lk.ijse.dto.BookDTO;
@@ -34,7 +37,7 @@ public class HomePageFormController {
 
     Pages pages = new PagesImpl();
 
-
+    HomeBO home = new HomeBOImpl();
 
     @FXML
     void viewBookDetailsOnAction(ActionEvent event) throws IOException {
@@ -42,15 +45,31 @@ public class HomePageFormController {
     }
 
     @FXML
-    public void saveRecordOnAction(ActionEvent actionEvent) {
-
+    void saveAuthorOnAction(ActionEvent event) {
         var authorDTO = new AuthorDTO();
         authorDTO.setName(txtAuthorName.getText());
 
+        boolean isSaved = home.saveAuthor(authorDTO);
+        if (isSaved){
+            new Alert(Alert.AlertType.INFORMATION,"Saved Author!").show();
+        }else {
+            new Alert(Alert.AlertType.ERROR,"Try Again!").show();
+        }
+    }
+
+    @FXML
+    void saveBookOnAction(ActionEvent event) {
         var bookDTO = new BookDTO();
         bookDTO.setTitle(txtBookTitle.getText());
         bookDTO.setPublicationYear(txtPublishedYear.getText());
         bookDTO.setPrice(Double.parseDouble(txtPrice.getText()));
+
+        boolean isSaved = home.saveBook(bookDTO);
+        if (isSaved){
+            new Alert(Alert.AlertType.INFORMATION,"Saved Book!").show();
+        }else {
+            new Alert(Alert.AlertType.ERROR,"Try Again!").show();
+        }
     }
 
 //    public void saveRecord(String author, String bookTitle, String publishedYear, double price){
