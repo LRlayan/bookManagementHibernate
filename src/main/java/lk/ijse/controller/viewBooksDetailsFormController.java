63b0ef80e49e222f1a2b2cef85a6e8f6f79a3378
LRLayan <rameshlayan4@gmail.com>
2024-02-14 03:24:07 +0530

    loadAllBook()

--- src/main/java/lk/ijse/controller/viewBooksDetailsFormController.java
+++ src/main/java/lk/ijse/controller/viewBooksDetailsFormController.java
@@ -1,10 +1,18 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.sun.javafx.scene.control.skin.FXVK;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import lk.ijse.bo.custom.HomeBO;
 import lk.ijse.bo.custom.impl.HomeBOImpl;
 import lk.ijse.dto.BookDTO;
+import lk.ijse.dto.tm.BookTM;
 
 import java.net.URL;
 import java.util.List;
@@ -12,9 +20,56 @@ import java.util.ResourceBundle;
 
 public class viewBooksDetailsFormController implements Initializable {
 
+    @FXML
+    private TableView<BookTM> bookTable;
+
+    @FXML
+    private TableColumn<?, ?> colAuthorId;
+
+    @FXML
+    private TableColumn<?, ?> colBookId;
+
+    @FXML
+    private TableColumn<?, ?> colBookTitle;
+
+    @FXML
+    private TableColumn<?, ?> colPrice;
+
+    @FXML
+    private TableColumn<?, ?> colPublishedYear;
+
+    @FXML
+    private TableColumn<?, ?> colUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colDelete;
+
     HomeBO home = new HomeBOImpl();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        setCellValueFactory();
+        loadAllBooks();
+    }
+
+    private void loadAllBooks() {
+        ObservableList<BookTM> obList = FXCollections.observableArrayList();
+
+        for (BookDTO bookDTO :home.findAllBook()){
+            obList.add(new BookTM(
+                    bookDTO.getId(),
+                    bookDTO.getTitle(),
+                    bookDTO.getPublicationYear(),
+                    bookDTO.getPrice(),
+                    bookDTO.getAuthor().getId(),
+                    new JFXButton("Update"),
+                    new JFXButton("Delete")
+            ));
+        }
+        bookTable.setItems(obList);
+    }
+
+    private void setCellValueFactory(){
 
     }
 
