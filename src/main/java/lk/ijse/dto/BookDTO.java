178a6b17e2b37330dc96339858d8e9c1c0d83612
LRLayan <rameshlayan4@gmail.com>
2024-02-14 01:12:01 +0530

    Author property

--- src/main/java/lk/ijse/dto/BookDTO.java
+++ src/main/java/lk/ijse/dto/BookDTO.java
@@ -1,5 +1,6 @@
 package lk.ijse.dto;
 
+import lk.ijse.entity.Author;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,5 @@ public class BookDTO {
     private String title;
     private String publicationYear;
     private double price;
+    private Author author;
 }
