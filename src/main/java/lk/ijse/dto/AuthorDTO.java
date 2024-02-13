75c3f05eb9b083826dc7861ab1322594848527ee
LRLayan <rameshlayan4@gmail.com>
2024-02-14 01:11:17 +0530

    country property

--- src/main/java/lk/ijse/dto/AuthorDTO.java
+++ src/main/java/lk/ijse/dto/AuthorDTO.java
@@ -1,8 +1,12 @@
 package lk.ijse.dto;
 
+import lk.ijse.entity.Book;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -10,4 +14,12 @@ import lombok.NoArgsConstructor;
 public class AuthorDTO {
     private int id;
     private String name;
+    private String country;
+    private List<Book> bookList;
+
+    public AuthorDTO(int id, String name, String country) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+    }
 }
