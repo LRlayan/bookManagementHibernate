a69c0caf74a5309233ecd365eedead8705008bc8
LRLayan <rameshlayan4@gmail.com>
2024-02-13 15:04:38 +0530

    saveAuthor()

--- src/main/java/lk/ijse/bo/custom/impl/HomeBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/HomeBOImpl.java
@@ -1,6 +1,23 @@
 package lk.ijse.bo.custom.impl;
 
 import lk.ijse.bo.custom.HomeBO;
+import lk.ijse.dao.custom.AuthorDAO;
+import lk.ijse.dao.custom.impl.AuthorDAOImpl;
+import lk.ijse.dto.AuthorDTO;
+import lk.ijse.dto.BookDTO;
+import lk.ijse.entity.Author;
 
 public class HomeBOImpl implements HomeBO {
+
+    AuthorDAO authorDAO = new AuthorDAOImpl();
+
+    @Override
+    public boolean saveAuthor(AuthorDTO authorDTO) {
+        return authorDAO.save(new Author(authorDTO.getName()));
+    }
+
+    @Override
+    public boolean saveBook(BookDTO bookDTO) {
+        return false;
+    }
 }
