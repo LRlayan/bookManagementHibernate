4f3ec1eda52f50f0946ff3fa63287bb68fd53ea3
LRLayan <rameshlayan4@gmail.com>
2024-02-14 01:13:08 +0530

    findAllAuthor()

--- src/main/java/lk/ijse/bo/custom/HomeBO.java
+++ src/main/java/lk/ijse/bo/custom/HomeBO.java
@@ -4,7 +4,12 @@ import lk.ijse.bo.SuperBO;
 import lk.ijse.dto.AuthorDTO;
 import lk.ijse.dto.BookDTO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface HomeBO extends SuperBO {
     boolean saveAuthor(AuthorDTO authorDTO);
     boolean saveBook(BookDTO bookDTO);
+
+    List<AuthorDTO> findAllAuthor();
 }
