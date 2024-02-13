e73d6b1160efce8f2129f099a52b2dcd0f08f683
LRLayan <rameshlayan4@gmail.com>
2024-02-13 15:04:07 +0530

    saveAuthor() , saveBook()

--- src/main/java/lk/ijse/bo/custom/HomeBO.java
+++ src/main/java/lk/ijse/bo/custom/HomeBO.java
@@ -1,6 +1,10 @@
 package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.AuthorDTO;
+import lk.ijse.dto.BookDTO;
 
 public interface HomeBO extends SuperBO {
+    boolean saveAuthor(AuthorDTO authorDTO);
+    boolean saveBook(BookDTO bookDTO);
 }
