edb883d2abfd22f20d4ccfdd66014af60095f487
LRLayan <rameshlayan4@gmail.com>
2024-02-15 00:40:15 +0530

    deleteBook()

--- src/main/java/lk/ijse/bo/custom/impl/ViewBooksDetailsBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/ViewBooksDetailsBOImpl.java
@@ -1,11 +1,15 @@
 package lk.ijse.bo.custom.impl;
 
+import lk.ijse.dao.custom.BookDAO;
+import lk.ijse.dao.custom.impl.BookDAOImpl;
 import lk.ijse.dto.AuthorDTO;
+import lk.ijse.entity.Book;
 
 import java.util.ArrayList;
 
 public class ViewBooksDetailsBOImpl implements lk.ijse.bo.custom.ViewBooksDetailsBO {
 
+    BookDAO bookDAO = new BookDAOImpl();
 
     @Override
     public boolean updateAuthor() {
@@ -15,6 +19,11 @@ public class ViewBooksDetailsBOImpl implements lk.ijse.bo.custom.ViewBooksDetail
     @Override
     public boolean deleteAuthor() {
         return false;
+    }
+
+    @Override
+    public boolean deleteBook(int id) {
+       return bookDAO.delete(id);
     }
 
     @Override
