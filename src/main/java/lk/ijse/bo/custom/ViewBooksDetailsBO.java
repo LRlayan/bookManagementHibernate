e4aa2488c42e79d6b25d1d3755a05f317d4bc712
LRLayan <rameshlayan4@gmail.com>
2024-02-15 00:40:22 +0530

    deleteBook()

--- src/main/java/lk/ijse/bo/custom/ViewBooksDetailsBO.java
+++ src/main/java/lk/ijse/bo/custom/ViewBooksDetailsBO.java
@@ -2,6 +2,7 @@ package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBO;
 import lk.ijse.dto.AuthorDTO;
+import lk.ijse.entity.Book;
 
 import java.util.ArrayList;
 
@@ -9,5 +10,6 @@ public interface ViewBooksDetailsBO extends SuperBO {
 
     boolean updateAuthor();
     boolean deleteAuthor();
+    boolean deleteBook(int id);
     ArrayList<AuthorDTO> getAllAuthors();
 }
