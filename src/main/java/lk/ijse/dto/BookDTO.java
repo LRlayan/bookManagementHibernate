2943734078200fa652a0d09c6a439e2fa42df833
LRLayan <rameshlayan4@gmail.com>
2024-02-15 01:07:21 +0530

    overload constructor

--- src/main/java/lk/ijse/dto/BookDTO.java
+++ src/main/java/lk/ijse/dto/BookDTO.java
@@ -14,9 +14,26 @@ public class BookDTO {
     private String publicationYear;
     private double price;
     private int authorId;
+    private int percentage;
 
     public BookDTO(String title) {
         this.title = title;
     }
 
+    public BookDTO(int id, int authorId, String title, String publicationYear, double price, int percentage) {
+        this.id = id;
+        this.authorId = authorId;
+        this.title = title;
+        this.publicationYear = publicationYear;
+        this.price = price;
+        this.percentage = percentage;
+    }
+
+    public BookDTO(int id, String title, String publicationYear, double price, int authorId) {
+        this.id = id;
+        this.title = title;
+        this.publicationYear = publicationYear;
+        this.price = price;
+        this.authorId = authorId;
+    }
 }
