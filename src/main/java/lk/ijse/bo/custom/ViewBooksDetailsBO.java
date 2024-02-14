9e68de4706a411857ba1c359ec8a6a80971ed912
LRLayan <rameshlayan4@gmail.com>
2024-02-14 15:02:01 +0530

    remove saveAuthor()

--- src/main/java/lk/ijse/bo/custom/ViewBooksDetailsBO.java
+++ src/main/java/lk/ijse/bo/custom/ViewBooksDetailsBO.java
@@ -6,7 +6,7 @@ import lk.ijse.dto.AuthorDTO;
 import java.util.ArrayList;
 
 public interface ViewBooksDetailsBO extends SuperBO {
-    boolean saveAuthor();
+
     boolean updateAuthor();
     boolean deleteAuthor();
     ArrayList<AuthorDTO> getAllAuthors();
