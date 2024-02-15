b9532c265cec82cf0e855378fa5b275b8e68c676
LRLayan <rameshlayan4@gmail.com>
2024-02-15 16:47:07 +0530

    loadAllAuthors()

--- src/main/java/lk/ijse/controller/viewBooksDetailsFormController.java
+++ src/main/java/lk/ijse/controller/viewBooksDetailsFormController.java
@@ -14,7 +14,9 @@ import lk.ijse.bo.custom.HomeBO;
 import lk.ijse.bo.custom.ViewBooksDetailsBO;
 import lk.ijse.bo.custom.impl.HomeBOImpl;
 import lk.ijse.bo.custom.impl.ViewBooksDetailsBOImpl;
+import lk.ijse.dto.AuthorDTO;
 import lk.ijse.dto.BookDTO;
+import lk.ijse.dto.tm.AuthorTM;
 import lk.ijse.dto.tm.BookTM;
 import lk.ijse.pages.Impl.PagesImpl;
 import lk.ijse.pages.Pages;
@@ -51,16 +53,77 @@ public class viewBooksDetailsFormController implements Initializable {
     @FXML
     private TableColumn<?, ?> colDelete;
 
-    HomeBO home = new HomeBOImpl();
+    @FXML
+    private TableView<AuthorTM> authorTable;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colAuthorName;
+
+    @FXML
+    private TableColumn<?, ?> colCountry;
+
+    @FXML
+    private TableColumn<?, ?> colAuthorDelete;
 
     ObservableList<BookTM> obList = FXCollections.observableArrayList();
-
+    ObservableList<AuthorTM> obListAuthor = FXCollections.observableArrayList();
     ViewBooksDetailsBO viewBooksDetails = new ViewBooksDetailsBOImpl();
+    HomeBO home = new HomeBOImpl();
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setCellValueFactory();
         loadAllBooks();
+        setCellValueFactoryInAuthor();
+        loadAllAuthor();
+    }
+
+    private void setCellValueFactoryInAuthor() {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colAuthorName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        colCountry.setCellValueFactory(new PropertyValueFactory<>("Country"));
+        colAuthorDelete.setCellValueFactory(new PropertyValueFactory<>("Delete"));
+    }
+
+    private void loadAllAuthor() {
+        List<AuthorDTO> authors = viewBooksDetails.findAllAuthors();
+        for (AuthorDTO authorDTO :authors){
+            obListAuthor.add(new AuthorTM(
+                    authorDTO.getId(),
+                    authorDTO.getName(),
+                    authorDTO.getCountry(),
+                    new JFXButton("Delete")
+            ));
+        }
+        buttonsDesign("obListAuthor");
+        authorTable.setItems(obListAuthor);
+
+        for (int i = 0; i < obListAuthor.size(); i++) {
+            int authorId = obListAuthor.get(i).getId();
+            obListAuthor.get(i).getDelete().setOnAction(event -> {
+                ButtonType yes = new ButtonType("Yes",ButtonBar.ButtonData.OK_DONE);
+                ButtonType no = new ButtonType("No",ButtonBar.ButtonData.CANCEL_CLOSE);
+
+                Optional<ButtonType> type = new Alert(Alert.AlertType.CONFIRMATION,"Are you sure delete Author",yes , no).showAndWait();
+                if (type.get().getText() == "Yes"){
+                    try {
+                       boolean isDelete = viewBooksDetails.deleteAuthor(authorId);
+                       if (isDelete){
+                           new Alert(Alert.AlertType.INFORMATION,"Delete Successfully").show();
+                       }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                       // new Alert(Alert.AlertType.ERROR,"Try Again!").show();
+                    }
+                }
+                authorTable.refresh();
+                loadAllAuthor();
+            });
+        }
     }
 
     private void loadAllBooks() {
@@ -73,10 +136,10 @@ public class viewBooksDetailsFormController implements Initializable {
                     bookDTO.getPrice(),
                     bookDTO.getAuthorId(),
                     new JFXButton("Update"),
-                   new JFXButton("Delete")
+                    new JFXButton("Delete")
             ));
         }
-        buttonsDesign();
+        buttonsDesign("obList");
         bookTable.setItems(obList);
 
         for (int i = 0; i < obList.size(); i++) {
@@ -84,10 +147,10 @@ public class viewBooksDetailsFormController implements Initializable {
                 Pages pages = new PagesImpl();
                 try {
                     pages.popUpWindow("/view/updateBook.fxml");
-                    bookTable.refresh();
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
+                bookTable.refresh();
             });
 
             int bookId = obList.get(i).getBookId();
@@ -99,10 +162,10 @@ public class viewBooksDetailsFormController implements Initializable {
                 if (type.get().getText() == "Yes"){
 
                     try{
-                            boolean isDelete = viewBooksDetails.deleteBook(bookId);
-                            if (isDelete) {
-                                new Alert(Alert.AlertType.INFORMATION, "Delete Successfully!").show();
-                            }
+                        boolean isDelete = viewBooksDetails.deleteBook(bookId);
+                        if (isDelete) {
+                             new Alert(Alert.AlertType.INFORMATION, "Delete Successfully!").show();
+                        }
                     }catch (Exception e){
                         new Alert(Alert.AlertType.ERROR,"Try Again").show();
                     }
@@ -113,12 +176,19 @@ public class viewBooksDetailsFormController implements Initializable {
         }
     }
 
-    private void buttonsDesign() {
-        for (int i = 0; i < obList.size(); i++) {
-             obList.get(i).getUpdate().setTextFill(Color.WHITE);
-             obList.get(i).getUpdate().setBackground(Background.fill(Color.GREEN));
-             obList.get(i).getDelete().setTextFill(Color.WHITE);
-             obList.get(i).getDelete().setBackground(Background.fill(Color.RED));
+    private void buttonsDesign(String name) {
+        if (name.equals("obList")) {
+            for (int i = 0; i < obList.size(); i++) {
+                obList.get(i).getUpdate().setTextFill(Color.WHITE);
+                obList.get(i).getUpdate().setBackground(Background.fill(Color.GREEN));
+                obList.get(i).getDelete().setTextFill(Color.WHITE);
+                obList.get(i).getDelete().setBackground(Background.fill(Color.RED));
+            }
+        } else {
+            for (int i = 0; i < obListAuthor.size(); i++) {
+                obListAuthor.get(i).getDelete().setTextFill(Color.WHITE);
+                obListAuthor.get(i).getDelete().setBackground(Background.fill(Color.RED));
+            }
         }
     }
 
