b30e1f17be889d177c673192f0a6843909aaa4e4
LRLayan <rameshlayan4@gmail.com>
2024-02-15 20:26:19 +0530

    allBooksByAuthor()

--- src/main/java/lk/ijse/dao/custom/AuthorDAO.java
+++ src/main/java/lk/ijse/dao/custom/AuthorDAO.java
@@ -3,5 +3,8 @@ package lk.ijse.dao.custom;
 import lk.ijse.dao.CrudDAO;
 import lk.ijse.entity.Author;
 
+import java.util.List;
+
 public interface AuthorDAO extends CrudDAO<Author> {
+    List<Object[]> allBooksByAuthor();
 }
