6fe5fe6ea85d79a1bfc4d4bfeb923e1a17afdcf4
LRLayan <rameshlayan4@gmail.com>
2024-02-15 16:45:15 +0530

    findAllAuthors() , findAllBook()

--- src/main/java/lk/ijse/bo/custom/ViewBooksDetailsBO.java
+++ src/main/java/lk/ijse/bo/custom/ViewBooksDetailsBO.java
@@ -2,14 +2,17 @@ package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBO;
 import lk.ijse.dto.AuthorDTO;
+import lk.ijse.dto.BookDTO;
 import lk.ijse.entity.Book;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface ViewBooksDetailsBO extends SuperBO {
 
     boolean updateAuthor();
-    boolean deleteAuthor();
+    boolean deleteAuthor(int authorId);
     boolean deleteBook(int id);
-    ArrayList<AuthorDTO> getAllAuthors();
+    ArrayList<AuthorDTO> findAllAuthors();
+    List<BookDTO> findAllBook();
 }
