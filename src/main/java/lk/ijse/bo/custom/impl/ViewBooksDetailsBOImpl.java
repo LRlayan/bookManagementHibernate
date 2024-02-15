e576659b952e7bd1842ca21bbe139948e1c5601d
LRLayan <rameshlayan4@gmail.com>
2024-02-15 16:46:38 +0530

    findAllBook() , findAllAuthors()

--- src/main/java/lk/ijse/bo/custom/impl/ViewBooksDetailsBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/ViewBooksDetailsBOImpl.java
@@ -1,15 +1,21 @@
 package lk.ijse.bo.custom.impl;
 
+import lk.ijse.dao.custom.AuthorDAO;
 import lk.ijse.dao.custom.BookDAO;
+import lk.ijse.dao.custom.impl.AuthorDAOImpl;
 import lk.ijse.dao.custom.impl.BookDAOImpl;
 import lk.ijse.dto.AuthorDTO;
+import lk.ijse.dto.BookDTO;
+import lk.ijse.entity.Author;
 import lk.ijse.entity.Book;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ViewBooksDetailsBOImpl implements lk.ijse.bo.custom.ViewBooksDetailsBO {
 
     BookDAO bookDAO = new BookDAOImpl();
+    AuthorDAO authorDAO = new AuthorDAOImpl();
 
     @Override
     public boolean updateAuthor() {
@@ -17,8 +23,8 @@ public class ViewBooksDetailsBOImpl implements lk.ijse.bo.custom.ViewBooksDetail
     }
 
     @Override
-    public boolean deleteAuthor() {
-        return false;
+    public boolean deleteAuthor(int authorId) {
+        return authorDAO.delete(authorId);
     }
 
     @Override
@@ -27,7 +33,20 @@ public class ViewBooksDetailsBOImpl implements lk.ijse.bo.custom.ViewBooksDetail
     }
 
     @Override
-    public ArrayList<AuthorDTO> getAllAuthors() {
+    public ArrayList<AuthorDTO> findAllAuthors() {
+        ArrayList<AuthorDTO> authorDTOS = new ArrayList<AuthorDTO>();
+        for (Author author :authorDAO.findAll()){
+            authorDTOS.add(new AuthorDTO(
+                    author.getId(),
+                    author.getName(),
+                    author.getCountry()
+            ));
+        }
+        return authorDTOS;
+    }
+
+    @Override
+    public List<BookDTO> findAllBook() {
         return null;
     }
 }
