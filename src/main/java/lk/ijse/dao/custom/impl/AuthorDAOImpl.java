1070d28aab7474bda0f3bd2e6aebc77e80e9f805
LRLayan <rameshlayan4@gmail.com>
2024-02-15 16:43:16 +0530

    loadAllAuthor()

--- src/main/java/lk/ijse/dao/custom/impl/AuthorDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/AuthorDAOImpl.java
@@ -1,8 +1,10 @@
 package lk.ijse.dao.custom.impl;
 
+import jakarta.persistence.Query;
 import lk.ijse.config.FactoryConfiguration;
 import lk.ijse.dao.custom.AuthorDAO;
 import lk.ijse.entity.Author;
+import lk.ijse.entity.Book;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -32,11 +34,24 @@ public class AuthorDAOImpl implements AuthorDAO {
 
     @Override
     public boolean delete(int id) {
-        return false;
+       Session session1 = FactoryConfiguration.getInstance().getSession();
+       Transaction transaction1 = session1.beginTransaction();
+
+     Author author = session1.get(Author.class,id);
+        for (Book book : author.getBookList()){
+            session1.delete(book);
+        }
+
+        session1.delete(author);
+        transaction1.commit();
+        session1.close();
+
+        return true;
     }
 
     @Override
     public List<Author> findAll() {
-        return null;
+        Session session1 = FactoryConfiguration.getInstance().getSession();
+        return session1.createQuery("FROM Author").list();
     }
 }
